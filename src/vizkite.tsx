7672fe9d7d266382bae1778d597eff18ffea2272
hypercodex <hypercodex@protonmail.com>
2020-09-01 04:43:43 -0700

    improve typings with generics

--- src/vizkite.tsx
+++ src/vizkite.tsx
@@ -4,27 +4,26 @@ import React, { useRef, useEffect } from 'react'
 
 export type TargetRef = React.MutableRefObject<HTMLDivElement>
 
-export type D3CallbackDataParameter = any[];
 
 
-export interface D3CallbackType {
-  (ref: HTMLDivElement, data: D3CallbackDataParameter): void;
+export interface D3CallbackFunc<D, O> {
+  (ref: HTMLDivElement, data: D, options?: O): void;
 }
-
 
 export interface D3HookFunction {
-  (
-    d3Callback: D3CallbackType,
-    data: D3CallbackDataParameter
-  ): React.MutableRefObject<HTMLDivElement>;
+  <D, O>(
+    d3Callback: D3CallbackFunc<D, O>,
+    data: D,
+    options?: O
+  ): TargetRef;
 }
 
 
-export const useD3: D3HookFunction = (d3Callback, data) => {
+export const useD3: D3HookFunction = (d3Callback, data, options) => {
   
-  const ref = useRef(null) as React.MutableRefObject<HTMLDivElement>;
+  const ref = useRef(null) as TargetRef;
   useEffect(() => {
-    d3Callback(ref.current, data);
+    d3Callback(ref.current, data, options);
 
     return () => {
       if (ref.current && ref.current.firstChild) {  
@@ -39,7 +38,7 @@ export const useD3: D3HookFunction = (d3Callback, data) => {
 
 
 export interface D3TargetBaseProps {
-  className: string;
+  className?: string;
 }
 
 export interface D3TargetProps extends D3TargetBaseProps { 
@@ -52,24 +51,22 @@ export const D3Target = (props: D3TargetProps): JSX.Element => (
 );
 
 
-export interface D3Props extends D3TargetBaseProps {
-  d3Callback: D3CallbackType;
-  data: D3CallbackDataParameter;
+interface D3ContainerProps<D, O> extends D3TargetBaseProps {
+  d3Callback: D3CallbackFunc<D, O>;
+  data: D;
+  options?: O;
 }
 
-export const D3Container = (props: D3Props): JSX.Element => {
+export const D3Container = <T, O, P extends D3ContainerProps<T, O>>(props: P): JSX.Element => {
   const { 
     d3Callback,
     data,
+    options
   } = props;
 
-  const forwardRef = useD3(d3Callback, data);
+  const forwardRef = useD3(d3Callback, data, options);
 
   return (
-      <D3Target {...{...props, forwardRef: forwardRef}} /> 
+      <D3Target {...{...props as P, forwardRef: forwardRef}} /> 
   );
 }
-
-
-
-
