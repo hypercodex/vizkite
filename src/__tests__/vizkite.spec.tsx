fe82add4fa33e3157ee2deb8b861cd166de1cb90
Wilcox <hypercodex@users.noreply.github.com>
2020-09-05 03:50:24 -0700

    target HTMLDivElement  (#6)
    * improve typings with generics
    
    * refactor types with generics; fit api usage patterns
    
    * update readme to reflect refactor with generics
    
    * add id prop to target
    
    * bump package version prior to release
    
    * add required id prop to component call

--- src/__tests__/vizkite.spec.tsx
+++ src/__tests__/vizkite.spec.tsx
@@ -17,7 +17,7 @@ describe("Test units of custom hook", () => {
     const mockD3Callback = jest.fn((ref, data) => {return null;});
     const { result } = renderHook(() => useD3(mockD3Callback, [1, 2, 3])); // Call our hook.
     const svg = mount(
-      <D3Target  className='test' forwardRef={result.current[0]} />);
+      <D3Target  id='test' className='test' forwardRef={result.current[0]} />);
     expect(svg.text()).toBe("")
     expect(mockD3Callback).toBeCalled()
   });
@@ -30,7 +30,7 @@ describe("Test units of custom hook", () => {
         ref.appendChild(childNode);
       }
     };
-    const svg = mount(<D3Container className='test' d3Callback={d3Callback} data={[1, 2, 3]}/>);
+    const svg = mount(<D3Container id='test' className='test' d3Callback={d3Callback} data={[1, 2, 3]}/>);
     expect(svg.html()).toContain('circle')
   });
 
